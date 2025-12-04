6195d20e3b9afc90e84703572882b7911f0a35b7
Dmitriy <jrdeath1337@gmail.com>
2025-12-04 15:50:30 +0300

    update script

--- ex4/ex4.sh
+++ ex4/ex4.sh
@@ -5,9 +5,9 @@ read -p "type file extension without dot: " ext 			#Second arg
 read -p "print the path to the directory: " dir 			#Third arg
 
 if [[ -d "$dir" ]]; then #Check existense directory
-	if ls "$dir" | grep -q -r -l "\.$ext$"; then 			# Check are there any files with this extension 
+	if ls -Rl "$dir" | grep -q "\.$ext$"; then	 		# Check are there any files with this extension 
 		touch "$PWD/$filename" 					# Create new file 
-		ls "$dir" | grep -r -l "\.$ext$" > "$PWD/$filename" 	# Outout redirection
+		ls -Rl "$dir" | grep "\.$ext$" > "$PWD/$filename" 	# Outout redirection
 		echo "You file in $PWD/$filename"			# Show way to the file
 	else	
 		echo "No file with this extension"			# Error
