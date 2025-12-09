edf7df7e13fdbc001199b9bd6f918b4f143a84c8
Dmitriy <jrdeath1337@gmail.com>
2025-12-09 14:14:00 +0300

    ex1 work version push

--- BASH_1_LESS_13/ex1/ex1.sh
+++ BASH_1_LESS_13/ex1/ex1.sh
@@ -1,45 +1,34 @@
 #!/usr/bin/bash
 
 read -p "Choose directory: " dir
-read -p "Choose extension: " ext
-read -p "Choose option timestamp(1), git hesh(2): " option
 
-case $option in
+for file in $(find "$dir" -type f 2>/dev/null); do
+	if [[ -f "$file" && "$file" == "*.log" ]]; then
 
-	1)
-	for file in $(find "$dir" -maxdepth 1 -name -type f ."$ext"  f 2>/dev/null); do
-		if [ -f "$file" ]; then
+        	timestamp=$(stat -c %Y "$file")
+        	formatted_timestamp=$(date -d "@$timestamp" +"%Y%m%d_%H%M%S")
 
-        		timestamp=$(stat -c %Y "$file")
-        		formatted_timestamp=$(date -d "@$timestamp" +"%Y%m%d_%H%M%S")
+       		name="${file%.*}"
+     		ext="${file##*.}"
 
-        		name="${file%.*}"
-    	   		ext="${file##*.}"
+   	 	new_name="${name}_${formatted_timestamp}.${ext}"
 
-   	 		new_name="${name}_${formatted_timestamp}.${ext}"
+  	 	#mv "$file" "$new_name"
+   		echo "Переименован: $file -> $new_name"
+		echo "$new_file"
+	elif [[ -f "$file" && "*.py" ]]; then
+		githash=$(git log -1 --format="%h")
 
-   	 		#mv "$file" "$new_name"
-  	 		echo "Переименован: $file -> $new_name"
-			echo "$new_file"
-		fi
-	done;;
+        	name="${file%.*}"
+    	   	ext="${file##*.}"
 
-	2)
-	for file in $(find "$dir" -name \."$ext" -maxdepth -1 -type f 2>/dev/null); do
-		if [ -f "$file" ]; then
+   	 	new_name="${name}.${ext}_$githash"
 
-        		githash=$(git log -1 --format="%h")
+   	 	#mv "$file" "$new_name"
+  	 	echo "Переименован: $file -> $new_name"
+		echo "$new_file"
+	else
+		echo "Files not found in this directory"
+	fi
+done
 
-        		name="${file%.*}"
-    	   		ext="${file##*.}"
-
-   	 		new_name="${name}.${ext}_$githash"
-
-   	 		#mv "$file" "$new_name"
-  	 		echo "Переименован: $file -> $new_name"
-			echo "$new_file"
-		fi
-	done;;
-	*)
-		echo "Error option"
-esac
