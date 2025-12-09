da2eb7786ef569021f9f67d4569b5503b98bdf12
Dmitriy <jrdeath1337@gmail.com>
2025-12-09 16:33:19 +0300

    finish working day (push result)

--- BASH_1_LESS_13/ex1/ex1.sh
+++ BASH_1_LESS_13/ex1/ex1.sh
@@ -2,33 +2,34 @@
 
 read -p "Choose directory: " dir
 
-for file in $(find "$dir" -type f 2>/dev/null); do
-	if [[ -f "$file" && "$file" == "*.log" ]]; then
+for file in $(find "$dir" -type f 2>/dev/null); do	#Запускаем все файлы в директории в цикл
+	if [[ "$file" == *.log ]]; then			#Проверяем что файл имеет расширение .log
 
-        	timestamp=$(stat -c %Y "$file")
-        	formatted_timestamp=$(date -d "@$timestamp" +"%Y%m%d_%H%M%S")
+        	timestamp=$(stat -c %Y "$file")		#Узнаем время изменения файла
+        	formatted_timestamp=$(date -d "@$timestamp" +"%Y%m%d_%H%M%S")	#Переводим секунды в человеческий формат
 
-       		name="${file%.*}"
-     		ext="${file##*.}"
+       		name="${file%.*}"	#Отделяем имя
+     		ext="${file##*.}"	#Отделяем расширение
 
-   	 	new_name="${name}_${formatted_timestamp}.${ext}"
+   	 	new_name="${name}_${formatted_timestamp}.${ext}"	#Формируем имя нового файла
 
-  	 	#mv "$file" "$new_name"
-   		echo "Переименован: $file -> $new_name"
-		echo "$new_file"
-	elif [[ -f "$file" && "*.py" ]]; then
-		githash=$(git log -1 --format="%h")
+  	 	mv "$file" "$new_name"		#Муваем файл для переименования
+   		echo "NEW FILE"
+		printf "%s\n %s" "$file" "--->" "$new_name"
+	elif [[ "$file" == *.py ]]; then
+		githash=$(git log -1 --format="%h")	#Получаем последний хеш коммита
 
         	name="${file%.*}"
     	   	ext="${file##*.}"
 
-   	 	new_name="${name}.${ext}_$githash"
+   	 	new_name="${name}_$githash.${ext}"
 
-   	 	#mv "$file" "$new_name"
-  	 	echo "Переименован: $file -> $new_name"
-		echo "$new_file"
+   	 	mv "$file" "$new_name"
+		echo "NEW FILE"
+		printf "%s\n %s" "$file" "--->" "$new_name"
 	else
-		echo "Files not found in this directory"
+		echo "ERROR"	#Выводит ошибку при отсутствии файлов нужного расширения
+		echo "$file not LOG or PY"
 	fi
 done
 
