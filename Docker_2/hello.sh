f9d02b427102da3cf764a0e18586efa9483a92ea
Dmitriy <jrdeath1337@gmail.com>
2026-02-02 14:16:01 +0300

    update Dockerfile and hello.sh

--- Docker_2/hello.sh
+++ Docker_2/hello.sh
@@ -2,8 +2,8 @@
 
 while true; do
   echo "I'm cat file hello.txt"
-  date | tee -a /logs/script.log
-  cat /hello/hello.txt | tee -a /logs/script.log
+  date | tee -a /home/jrdeath/logs/script.log
+  cat /home/jrdeath/hello.txt | tee -a /home/jrdeath/logs/script.log
   sleep 10
 done
 
