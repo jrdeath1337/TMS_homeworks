c57e3b862b4b3dac23601dd82420dc4951e3743c
Dmitriy <jrdeath1337@gmail.com>
2026-02-06 15:36:46 +0300

    start python homework

---
+++ Python_1/homework.py
@@ -0,0 +1,12 @@
+import os
+user = os.environ.get('USER')
+print(f"Hello user {user} \nGood day for you\n")
+
+a = int(input("Input first num: "))
+b = int(input("Input second num: "))
+print(f"info: \na+b result:({a+b})\n")
+
+
+name = input("Enter your name: ")
+print(f"Your name {name}")
+
