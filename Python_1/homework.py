003d3ded91c88ba0707d164a8788488e0c81b3cd
Dmitriy <jrdeath1337@gmail.com>
2026-02-09 14:38:13 +0300

    Add comments

--- Python_1/homework.py
+++ Python_1/homework.py
@@ -1,51 +1,52 @@
-print("info: Greetings with user")
+#Задания указаны в сообщении info, пример: "info: ex*"
+print("info: Greetings with user: ex 4")
 import os
 user = os.environ.get('USER')
 print(f"Hello user {user} \nGood day for you\n")
 
-print("info: simple sum calculate ")
+print("info: simple sum calculate: ex 5")
 a = int(input("Input first num: "))
 b = int(input("Input second num: "))
-print(f"info: \nnum A + num B result:({a+b})\n")
+print(f"Result: Num A + num B result:({a+b})\n")
 
-print("info: Recognize your name ")
+print("info: Recognize your name: ex6 ")
 name = input("Enter your name: ")
-print(f"Your name {name}, hello\n")
+print(f"Result: Your name {name}, hello\n")
 
-print("Work with cycle")
+print("Work with cycle: ex7")
 for i in range(10):
-    print(f"{i}")
+    print(f"Result: {i}")
 print("")
 
-print("Recognize your age")
+print("Recognize your age: ex8")
 age = int(input("Enter your age: "))
-print(f"Your age {age}")
+print(f"Result: Your age {age}")
 if age >= 18:
     print(f"You adalt\n")
 else:
     print(f"You yang\n")
 
-print("info: simple multipli calculate")
+print("info: simple multipli calculate: ex9")
 c = int(input("Input first num: "))
 d = int(input("Input second num: "))
-print(f"info: \n num A * num B result:({c*d})\n")
+print(f"Result: Num A * num B result:({c*d})\n")
 
-print("info: simple square calculate ")
+print("info: simple square calculate: ex11 ")
 f = int(input("Input num for squared: "))
-print(f"info: \nnum in squared result:({f*f})\n")
+print(f"Result: Num in squared result:({f*f})\n")
 
-print("info: 5 multiplication table")
+print("info: 5 multiplication table: ex12")
 for x in range(11):
-    print(f"result: 5*{x}={x*5}\n")
+    print(f"Result: 5*{x}={x*5}")
 print("")
 
-print("info: simpe avarage calculated") 
+print("info: simpe avarage calculated: ex13") 
 y = int(input("Input first num: "))
 z = int(input("Input second num: "))
 n = y+z
 p = n/len([y,z])
-print(f"info: avarage: {p}\n")
+print(f"Result: Average: {p}\n")
 
-print("info: calculating the first letter")
+print("info: calculating the first letter: ex10")
 word = input("Input word: ")
-print(f"First letter {word} is {word[0]}\n")
+print(f"Result: First letter {word} is {word[0]}\n")
