7b058e0d4fc682428a0c2faa6fbe81cfc162b086
Dmitriy <jrdeath1337@gmail.com>
2026-02-09 15:19:52 +0300

    update v1, add menu

--- Python_1/homework.py
+++ Python_1/homework.py
@@ -1,52 +1,62 @@
-#Задания указаны в сообщении info, пример: "info: ex*"
-print("info: Greetings with user: ex 4")
-import os
-user = os.environ.get('USER')
-print(f"Hello user {user} \nGood day for you\n")
+#Просто выберите задание для проверки по его номеру в ДЗ
+exersice = int(input("4 - hello\n5 - a+b\n6 - name\n7 - for(1-10)\n8 - age\n9 - a*b\n10 - first letter\n11 - squared\n12 - multiplication table\n13 - average\nSelect exersice: "))
+match exersice:
+    case 4:
+        print("info: Greetings with user")
+        import os
+        user = os.environ.get('USER')
+        print(f"Hello user {user} \nGood day for you\n")
 
-print("info: simple sum calculate: ex 5")
-a = int(input("Input first num: "))
-b = int(input("Input second num: "))
-print(f"Result: Num A + num B result:({a+b})\n")
+    case 5:
+        print("info: simple sum calculate")
+        a = int(input("Input first num: "))
+        b = int(input("Input second num: "))
+        print(f"Result: Num A + num B result:({a+b})\n")
 
-print("info: Recognize your name: ex6 ")
-name = input("Enter your name: ")
-print(f"Result: Your name {name}, hello\n")
+    case 6:
+        print("info: Recognize your name")
+        name = input("Enter your name: ")
+        print(f"Result: Your name {name}, hello\n")
 
-print("Work with cycle: ex7")
-for i in range(10):
-    print(f"Result: {i}")
-print("")
+    case 7:
+        print("Work with cycle")
+        for i in range(10):
+            print(f"Result: {i}")
 
-print("Recognize your age: ex8")
-age = int(input("Enter your age: "))
-print(f"Result: Your age {age}")
-if age >= 18:
-    print(f"You adalt\n")
-else:
-    print(f"You yang\n")
+    case 8:
+        print("Recognize your age")
+        age = int(input("Enter your age: "))
+        print(f"Result: Your age {age}")
+        if age >= 18:
+            print(f"You adalt\n")
+        else:
+            print(f"You yang\n")
 
-print("info: simple multipli calculate: ex9")
-c = int(input("Input first num: "))
-d = int(input("Input second num: "))
-print(f"Result: Num A * num B result:({c*d})\n")
+    case 9:
+        print("info: simple multipli calculate")
+        c = int(input("Input first num: "))
+        d = int(input("Input second num: "))
+        print(f"Result: Num A * num B result:({c*d})\n")
 
-print("info: simple square calculate: ex11 ")
-f = int(input("Input num for squared: "))
-print(f"Result: Num in squared result:({f*f})\n")
+    case 11:
+        print("info: simple square calculate")
+        f = int(input("Input num for squared: "))
+        print(f"Result: Num in squared result:({f*f})\n")
 
-print("info: 5 multiplication table: ex12")
-for x in range(11):
-    print(f"Result: 5*{x}={x*5}")
-print("")
+    case 12:
+        print("info: 5 multiplication table")
+        for x in range(11):
+            print(f"Result: 5*{x}={x*5}")
 
-print("info: simpe avarage calculated: ex13") 
-y = int(input("Input first num: "))
-z = int(input("Input second num: "))
-n = y+z
-p = n/len([y,z])
-print(f"Result: Average: {p}\n")
+    case 13:
+        print("info: simpe average calculated") 
+        y = int(input("Input first num: "))
+        z = int(input("Input second num: "))
+        n = y+z
+        p = n/len([y,z])
+        print(f"Result: Average: {p}\n")
 
-print("info: calculating the first letter: ex10")
-word = input("Input word: ")
-print(f"Result: First letter {word} is {word[0]}\n")
+    case 10:
+        print("info: calculating the first letter")
+        word = input("Input word: ")
+        print(f"Result: First letter {word} is {word[0]}\n")
