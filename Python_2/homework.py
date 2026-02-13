52193595d486d10f449acfaeb1ec2715b4d52796
Dmitriy <jrdeath1337@gmail.com>
2026-02-13 15:28:43 +0300

    add second ex

--- Python_2/homework.py
+++ Python_2/homework.py
@@ -1,5 +1,6 @@
 import os
 import re
+import string
 
 exercise = int(input("Выберите номер задания для проверки: "))
 match exercise:
@@ -16,3 +17,11 @@ match exercise:
                 else:
                     with open('access_adresses.log', 'a', encoding='utf-8') as log:
                         log.write(f"Adress: {ip_ping} NOT ACCESS\n")
+    case 2:
+        cli_input = input("Enter string: ")
+        upper_char = "".join([up_char for up_char in cli_input if up_char.isupper()])
+        lower_char = "".join([low_char for low_char in cli_input if low_char.islower()])
+        digital_char = "".join([dig_char for dig_char in cli_input if dig_char.isdigit()])
+        punctuation_char = "".join([punc_char for punc_char in cli_input if punc_char in string.punctuation])
+        print(f"Upper character text: {upper_char}\n Character count: ", len(upper_char), f"\nLower character text: {lower_char}\n Character count: ", len(lower_char), f"\nDigital character: {digital_char}\n Character count: ", len(digital_char), f"\nPunctuation character: {punctuation_char}\n Character count: ", len(punctuation_char))
+
